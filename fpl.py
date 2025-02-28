ea3c2cd5ee1315e944bac064c050914dceefd706
rik305 <riktt5@gmail.com>
2025-02-28 06:59:52 -0500

    Updated script, added function to return team in different formations

--- fpl.py
+++ fpl.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import requests, json
 from pprint import pprint
 import pandas as pd
@@ -14,9 +15,6 @@ players = pd.json_normalize(players)
 teams = pd.json_normalize(teams)
 
 
-players.set_index('id', inplace=True)
-teams.set_index('id', inplace=True)
-
 # Constants for positions
 GOALKEEPER = 1
 DEFENDER = 2
@@ -24,7 +22,7 @@ MIDFIELDER = 3
 FORWARD = 4
 
 # Define your budget and number of players needed per position
-budget = 1000  
+budget = 1040 
 team_size = 15
 gk_size = 2
 def_size = 5
@@ -39,7 +37,11 @@ midfielders = []
 forwards = []
 
 players.merge(teams, left_on='team', right_index=True, suffixes=('', '_team'))
-players = players[["first_name","second_name","total_points","points_per_game","form","now_cost","selected_by_percent","chance_of_playing_this_round","element_type","team"]]
+
+for i in players.columns:
+    print(i)
+
+players = players[["first_name","second_name","total_points","points_per_game","form","now_cost","selected_by_percent","chance_of_playing_this_round","element_type","team","id"]]
 
 players['name'] = players["first_name"] + " " + players["second_name"]
 
@@ -70,11 +72,12 @@ df = pd.DataFrame(defenders)
 md = pd.DataFrame(midfielders)
 fw = pd.DataFrame(forwards)
 
-for i in ["total_points","ppg","form","selected_by_percent"]:
-    gk.sort_values(by=i, ascending=False, inplace=True)
-    df.sort_values(by=i, ascending=False, inplace=True)
-    md.sort_values(by=i, ascending=False, inplace=True)
-    fw.sort_values(by=i, ascending=False, inplace=True)
+constraints = ["ppg","form","total_points","selected_by_percent"]
+for i in range(len(constraints)):
+    gk.sort_values(by=constraints[i], ascending=False, inplace=True)
+    df.sort_values(by=constraints[i], ascending=False, inplace=True)
+    md.sort_values(by=constraints[i], ascending=False, inplace=True)
+    fw.sort_values(by=constraints[i], ascending=False, inplace=True)
     for a in range(len(gk)):
         gk.loc[gk.index[a], 'count'] += a
     for a in range(len(df)):
@@ -101,36 +104,99 @@ cost = 0
 i = 0
 c = 0
 
-while len(team) != 5:
+while len(team) != 3:
     player = players.iloc[i]
     if (positions[player['element_type']-1] < [0,gk_size, def_size, mid_size, fwd_size][player['element_type']] 
     and teams[player['team']] < max_per_team):
-        team.append([player.name, player['element_type']])
+        team.append([player.name, player['element_type'],player['id']])
         cost += player['now_cost']
         positions[player['element_type']-1] += 1
         teams[player['team']] += 1
         c+=1
-        i+=1
+        i+=1 
 
 budget -= cost
+s = 2
 while len(team) != 15:
-    pavg = budget/(16-c)
+    pavg = budget/(15-c)
     player = players.iloc[i]
     if (positions[player['element_type']-1] < [0,gk_size, def_size, mid_size, fwd_size][player['element_type']] 
-    and teams[player['team']] < max_per_team and pavg>=player['now_cost']):
-        team.append([player.name, player['element_type']])
+    and teams[player['team']] < max_per_team and pavg + s>=player['now_cost'] and player.name not in [x[0] for x in team]):
+        team.append([player.name, player['element_type'],player['id']])
         cost += player['now_cost']
         budget -= player['now_cost']
         positions[player['element_type']-1] += 1
         teams[player['team']] += 1
         c += 1
+        i = 3
+        s *= -1
         
     i += 1
 
-team = sorted(team, key=lambda x: x[1])
-
+team = sorted(team, key=lambda x: 1000*x[1] + players.loc[x[0]]['count'])
+print("----------------------------------")
+print("Team:")
 for i in team:
-    print (players.loc[i[0]])
+    print (i[0], players.loc[i[0]]['count'])
+    print (players.loc[i[0]]['total_points'])
 
-for i in [0,2,3,4,7,8,9,10,11,12,13,14]:
-    print (players.loc[team[i][0]])
+print("Total Cost:", cost)
+print("Remaining Budget:", budget)
+
+tp = 0
+def tp_352(team):
+    tp = 0
+    s = ""
+    print(("3-5-2:").center(100))
+    print("\n")
+    for i in [0,2,3,4,7,8,9,10,11,12,13]:
+        name = team[i][0].split()
+        s+=name[0][0]+". "+name[-1]+"  "
+        if (i in [0,4,11,13]):
+            print(s.center(100), "\n")
+            s = ''
+        tp += players.loc[team[i][0]]['total_points']
+    print(("Total Points: " + (str(tp))).center(100))
+    print("\n")
+    return tp
+
+def tp_442(team):
+    tp = 0
+    s = ""
+    print(("4-4-2:").center(100))
+    print("\n")
+    for i in [0,2,3,4,5,7,8,9,10,12,13]:
+        name = team[i][0].split()
+        s+=name[0][0]+". "+name[-1]+"  "
+        if (i in [0,5,10,13]):
+            print(s.center(100), "\n")
+            s = ''
+        tp += players.loc[team[i][0]]['total_points']
+    print(("Total Points: " + (str(tp))).center(100))
+    print("\n")
+    return tp
+
+
+
+def tp_343(team):
+    tp = 0
+    s = ""
+    print(("3-4-3:").center(100))
+    print("\n")
+    for i in [0,2,3,4,7,8,9,10,12,13,14]:
+        name = team[i][0].split()
+        s+=name[0][0]+". "+name[-1]+"  "
+        if (i in [0,4,10,14]):
+            print(s.center(100), "\n")
+            s = ''
+        tp += players.loc[team[i][0]]['total_points']
+    print(("Total Points: " + (str(tp))).center(100))
+    print("\n")
+    return tp
+
+def get_gameweek_history(player_id):
+    r = requests.get(
+            base_url + 'element-summary/' + str(player_id) + '/'
+    ).json()
+    df = pd.json_normalize(r['history'])
+    return df 
